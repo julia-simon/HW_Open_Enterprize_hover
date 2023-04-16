8633759ab3fe1bd0e760929acc82d46da6938fc1
Julia Simon <juli.simonova@gmail.com>
2023-04-16 14:19:26 +0300

    fixed name

--- src/test/java/com/github/OpenEnterprizeHover.java
+++ src/test/java/com/github/OpenEnterprizeHover.java
@@ -1,6 +1,6 @@
 package com.github;
 
-import com.codeborne.selenide.Configuration;
+
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,9 +11,9 @@ import static com.codeborne.selenide.Selenide.*;
 public class OpenEnterprizeHover {
 
     @Test
-    void OpenEnterprizeHoverTest() {
+    void openEnterprizeHoverTest() {
         open("https://github.com/");
-        $(".d-lg-flex.list-style-none").$(byText("Solutions")).hover();
+        $(byText("Solutions")).hover();
         $(byTagAndText("a", "Enterprise")).click();
         $("h1.h1-mktg").shouldHave(text("Build like the best"));
 
